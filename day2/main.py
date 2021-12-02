053d2fcc423c676c74b48344bf8f955e85b02495
Peter Remøy Paulsen <petrepa@Peters-MBP.localdomain>
2021-12-02 19:04:05 +0100

    Completed part 1 of day 2

---
+++ day2/main.py
@@ -0,0 +1,46 @@
+
+
+def main():
+    depth = 0
+    postition = 0
+
+    forward = []
+    down = []
+    up = []
+
+    with open('list.txt') as f:
+        lines = [line.rstrip() for line in f]
+
+    for i in range(len(lines)):
+        if "forward" in lines[i]:
+            forward.append(lines[i][-1])
+            forward_int = list(map(int, forward))
+        if "down" in lines[i]:
+            down.append(lines[i][-1])
+            down_int = list(map(int, down))
+        if "up" in lines[i]:
+            up.append(lines[i][-1])
+            up_int = list(map(int, up))
+
+    forward_sum = sum(forward_int)
+    down_sum = sum(down_int)
+    up_sum = sum(up_int)
+
+    print("Sum of forwards: " + str(forward_sum))
+    print("Sum of downs: " + str(down_sum))
+    print("Sum of ups: " + str(up_sum))
+
+    depth = down_sum - up_sum
+
+    print("We're at depth: " +str(depth))
+
+    hordep = forward_sum * depth
+
+    return_string = "Final horizontal mulitplied by depth = " + str(hordep)
+
+    print(return_string)
+
+    return return_string
+
+if __name__ == "__main__":
+    main()
