7215fff51df6540abb1121bdfc8f1710b1f8c1c6
Peter Remøy Paulsen <petrepa@Peters-MBP.localdomain>
2021-12-02 18:24:33 +0100

    Apparently there was a part 2 as well. Solved that one too

--- day1/main.py
+++ day1/main.py
@@ -1,8 +1,7 @@
 
 
-def main():
+def part_1():
     increased = 0
-    print("Hello World!")
     
     with open('list.txt') as f:
         lines = [line.rstrip() for line in f]
@@ -14,7 +13,25 @@ def main():
         if numbers[i] > numbers[i-1]:
             increased += 1
 
-    print(increased)
+    print("Part 1: " + str(increased))
+
+def part_2():
+    increased = 0
+    
+    with open('list.txt') as f:
+        lines = [line.rstrip() for line in f]
+    
+    numbers = list(map(int, lines))
+    
+    numbers_sum = []
+
+    for i in range(len(numbers)):
+        numbers_sum.append(sum(numbers[i:i+3]))
+        if numbers_sum[i] > numbers_sum[i-1]:
+            increased += 1
+
+    print("Part 2: " + str(increased))
 
 if __name__ == "__main__":
-    main()
+    part_1()
+    part_2()
