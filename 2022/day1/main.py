af6e14b69a3a61af38ea88d21c380ba4de9efa58
Peter Remøy Paulsen <peter.remoy.paulsen@gmail.com>
2022-12-14 10:22:02 +0100

    Finished part 2 of day 1 of 2022

--- 2022/day1/main.py
+++ 2022/day1/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # open the file with all of the calories
 f = open("elf_calories.txt", "r")
 
@@ -20,3 +22,27 @@ print(
         max(total_calories_for_a_elf)
     )
 )
+
+
+def top_in_list(N: int, search_list: list):
+    a = np.array(search_list)
+    index = np.argpartition(a, -N)[-N:]
+
+    return a[index]
+
+
+def print_top_elves(top_list):
+    for index, item in enumerate(top_list):
+        print("              Elf {0}: {1} calories".format(index, item))
+
+
+def total_of_top_list(top_list):
+    return sum(top_list)
+
+
+print("Top 3 elves:")
+top_list = top_in_list(3, total_calories_for_a_elf)
+
+print_top_elves(top_list)
+
+print("Sum of top 3 elves: {0} calories".format(total_of_top_list(top_list)))
