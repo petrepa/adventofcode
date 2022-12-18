30434ac494069a0ab0eb90ba5824221c53604198
Peter Remøy Paulsen <peter.remoy.paulsen@gmail.com>
2022-12-18 16:16:18 +0100

    Solved part 2 of day 4

--- 2022/day4/main_2.py
+++ 2022/day4/main_2.py
@@ -30,3 +30,16 @@ def intersection_of_ranges(a: int, b: int, c: int, d: int):
 sum_of_overlaps = sum(1 for i in range(len(pairs)) if intersection_of_ranges(*pairs[i]))
 
 print(sum_of_overlaps)
+
+
+def overlap_at_all(a: int, b: int, c: int, d: int):
+    if a <= c and b >= c:
+        return True
+    if c <= a and d >= a:
+        return True
+    else:
+        return False
+
+
+sum_of_overlaps = sum(1 for i in range(len(pairs)) if overlap_at_all(*pairs[i]))
+print(sum_of_overlaps)
